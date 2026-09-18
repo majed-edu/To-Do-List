c11abdba3109bf328562d9d35c7b2757c9c32f46
ماجد حسين صالح قايد الحدي <23160077@su.edu.ye>
2026-09-19 01:16:47 +0300

    Refactor App component to manage todos and update UI structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
-import React from "react";
+import {useRef, useState} from "react";
+import "./App.css";
 
 function App() {
+
+  const [todos, setTodos] = useState([]);
+
+  const inputRef = useRef();
+
+  const handleAddTodo = () => {
+    const newTodo = inputRef.current.value;
+    // setTodos((prevTodos) => [...prevTodos, newTodo]);
+    setTodos([...todos, newTodo]);
+    inputRef.current.value = "";
+  }
+
   return (
-    <div>
-      <h1>To-Do List</h1>
+    <div className="App">
+      <h2 >To-Do List</h2>
+
+
+      <ul>
+        {todos.map((item) => (
+          <li>
+            {item}
+          </li>
+        ))}
+
+      </ul>
+      <input ref={inputRef} placeholder="enter item ..."/>
+
+      <button onClick={handleAddTodo}>
+        Add
+      </button>
     </div>
   );
 }
